badc844feb68e1bd2f88262e8ebc25556dad7c5b
Sasha1239 <68199637+Sasha1239@users.noreply.github.com>
2020-10-16 21:40:46 +0300

    Add files via upload

---
+++ lesson6_step4.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time 
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_tag_name("input[name=first_name]")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name("last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name("form-control city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id("country")
+    input4.send_keys("Russia")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
