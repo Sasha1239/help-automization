4d5f3831f718eedbd72853d333296dd9ab5d4b36
pen <pen@stacksoft.ru>
2020-10-18 16:18:02 +0300

    fixed input3

--- lesson6_step4.py
+++ lesson6_step4.py
@@ -31,7 +31,7 @@ try:
     input1.send_keys("Ivan")
     input2 = browser.find_element_by_name("last_name")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name("form-control city")
+    input3 = browser.find_element_by_class_name("form-control.city")
     input3.send_keys("Smolensk")
     input4 = browser.find_element_by_id("country")
     input4.send_keys("Russia")
