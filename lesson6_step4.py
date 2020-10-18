f1061444c74d8fc7f257e1e79da2e7238fba7ced
pen <pen@stacksoft.ru>
2020-10-18 15:31:20 +0300

    поиск полей по xpath

--- lesson6_step4.py
+++ lesson6_step4.py
@@ -1,13 +1,30 @@
+from enum import Enum
+
 from selenium import webdriver
 import time 
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+url: str = "http://suninjuly.github.io/simple_form_find_task.html"
+
+xpath_pattern: str = "//input[@name='{input_type}']"
+
+xpath_pattern2: str = "//div[@class='form-group' and child::label[text()='City:*']]/input[@name='firstname']"
+
+
+class InputType(Enum):
+    first_name: str = 'first_name'
+    last_name: str = 'last_name'
+    city: str = 'firstname'
+
+    def __str__(self):
+        return self.value
+
 
 try:
-    browser = webdriver.Chrome()
-    browser.get(link)
+    browser = webdriver.Firefox()
+    browser.get(url)
 
-    input1 = browser.find_element_by_tag_name("input[name=first_name]")
+    # input1 = browser.find_element_by_tag_name("input[name=first_name]")
+    input1 = browser.find_element_by_xpath(xpath_pattern.format(input_type=InputType.first_name))
     input1.send_keys("Ivan")
     input2 = browser.find_element_by_name("last_name")
     input2.send_keys("Petrov")
